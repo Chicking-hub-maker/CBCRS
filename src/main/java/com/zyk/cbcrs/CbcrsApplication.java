e59c9d5ef58c489010118ffb83d1cccb23f96821
Chicking-hub-maker <z3158387058@outlook.com>
2025-11-21 14:51:42 +0800

    CBCRSinit

---
+++ src/main/java/com/zyk/cbcrs/CbcrsApplication.java
@@ -0,0 +1,13 @@
+package com.zyk.cbcrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbcrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbcrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyk/cbcrs/CbcrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyk.cbcrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbcrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
